4236dc4628673d88c16b76cf58846ff14747f2c2
mdasari823 <mdasari@cs.stonybrook.edu>
2020-03-01 22:50:01 -0500

    adding laplacin

--- laplacian/bm/train.sh
+++ laplacian/bm/train.sh
@@ -7,26 +7,26 @@ hier=$1
 modeldir=model
 
 train="/home/mallesh/deepvideo/data/train"
-eval="/home/mallesh/deepvideo/data/vtl/eval"
+eval="/home/mallesh/deepvideo/data/eval"
 train_mv="/home/mallesh/deepvideo/data/train_mv"
-eval_mv="/home/mallesh/deepvideo/data/vtl/eval_mv"
+eval_mv="/home/mallesh/deepvideo/data/eval_mv"
 
 if [[ ${hier} == "0" ]]; then
   distance1=6
   distance2=6
-  bits=2
+  bits=16
   encoder_fuse_level=1
   decoder_fuse_level=1
 elif [[ ${hier} == "1" ]]; then
   distance1=3
   distance2=3
-  bits=2
+  bits=16
   encoder_fuse_level=2
   decoder_fuse_level=3
 elif [[ ${hier} == "2" ]]; then
   distance1=1
   distance2=2
-  bits=2
+  bits=8
   encoder_fuse_level=1
   decoder_fuse_level=1
 else
@@ -39,10 +39,13 @@ fi
 # when using a big evaluation dataset.
 # (for the demo data it's okay.)
 
-#--load-model-name "demo" --load-iter 15000 \
 
-python3 -u train_v3.py \
+#  --load-model-name "wunet_2:128_3:128_16" \
+#  --load-iter 95000 \
+
+python3 -u train.py \
   --train ${train} \
+  --iterations 10 \
   --eval ${eval} \
   --train-mv ${train_mv} \
   --eval-mv ${eval_mv} \
@@ -51,9 +54,8 @@ python3 -u train_v3.py \
   --v-compress --warp --stack --fuse-encoder \
   --bits ${bits} \
   --distance1 ${distance1} --distance2 ${distance2} \
-  --max-train-iters 30000 \
-  --checkpoint-iters 5000 \
-  --eval-iters 1000 \
-  --load-model-name "demo" --load-iter 30000 \
+  --max-train-iters 20000 \
+  --save-model-name "wunet_2:128_3:128_16" \
   --save-out-img \
-  --batch-size 2
+  --save-codes \
+  --eval-iters 5000
--- laplacian/em/first_model.sh
+++ laplacian/em/first_model.sh
@@ -6,27 +6,27 @@ hier=$1
 
 modeldir=model
 
-train="/home/mallesh/deepvideo/data/train"
-eval="/home/mallesh/deepvideo/data/vtl/eval"
-train_mv="/home/mallesh/deepvideo/data/train_mv"
-eval_mv="/home/mallesh/deepvideo/data/vtl/eval_mv"
+train="data/train"
+eval="data/eval"
+train_mv="data/train_mv"
+eval_mv="data/eval_mv"
 
 if [[ ${hier} == "0" ]]; then
   distance1=6
   distance2=6
-  bits=2
+  bits=16
   encoder_fuse_level=1
   decoder_fuse_level=1
 elif [[ ${hier} == "1" ]]; then
   distance1=3
   distance2=3
-  bits=2
+  bits=16
   encoder_fuse_level=2
   decoder_fuse_level=3
 elif [[ ${hier} == "2" ]]; then
   distance1=1
   distance2=2
-  bits=2
+  bits=8
   encoder_fuse_level=1
   decoder_fuse_level=1
 else
@@ -39,9 +39,8 @@ fi
 # when using a big evaluation dataset.
 # (for the demo data it's okay.)
 
-#--load-model-name "demo" --load-iter 15000 \
 
-python3 -u train_v3.py \
+python -u get_binarizer_output.py \
   --train ${train} \
   --eval ${eval} \
   --train-mv ${train_mv} \
@@ -51,9 +50,6 @@ python3 -u train_v3.py \
   --v-compress --warp --stack --fuse-encoder \
   --bits ${bits} \
   --distance1 ${distance1} --distance2 ${distance2} \
-  --max-train-iters 30000 \
-  --checkpoint-iters 5000 \
-  --eval-iters 1000 \
-  --load-model-name "demo" --load-iter 30000 \
-  --save-out-img \
-  --batch-size 2
+  --max-train-iters 1 \
+  --batch-size 1 \
+  --save-out-img
